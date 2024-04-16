9bb967a70fa53002fe6fa2aaca926427a81cb08e
Lupau Remus <lupau.remus15@gmail.com>
2024-04-16 22:42:57 +0300

    signin update

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,8 +1,8 @@
-const handleSignIn = (req, res, db, bcrypt) => {
+const handleSignIn = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json("Invalid credentials.");
+    return res.status(400).json("incorrect form submission");
   }
 
   db.select("email", "hash")
@@ -10,24 +10,20 @@ const handleSignIn = (req, res, db, bcrypt) => {
     .where("email", "=", email)
     .then((data) => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
-
       if (isValid) {
-        db.select("*")
+        return db
+          .select("*")
           .from("users")
-          .where("email", "=", data[0].email)
+          .where("email", "=", email)
           .then((user) => {
             res.json(user[0]);
           })
-          .catch((err) => {
-            res.status(400).json("Unable to sign in.");
-          });
+          .catch((err) => res.status(400).json("unable to get user"));
       } else {
-        res.status(400).json("Wrong credentials.");
+        res.status(400).json("wrong credentials");
       }
     })
-    .catch((err) => {
-      res.status(400).json("Wrong credentials.");
-    });
+    .catch((err) => res.status(400).json("wrong credentials"));
 };
 
 module.exports = {
