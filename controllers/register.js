9d40651803144bffb91e473173e42dff7c1f0622
Lupau Remus <lupau.remus15@gmail.com>
2024-10-15 11:08:36 +0300

    authentication with JWT done

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,9 @@
+const jwt = require("jsonwebtoken");
+
 const handleRegister = (req, res, db, bcrypt, saltRounds) => {
   const { name, email, password } = req.body;
+
+  const JWT_SECRET = process.env.JWT_SECRET;
 
   if (!name || !email || !password) {
     return res.status(400).json("Invalid credentials.");
@@ -24,7 +28,15 @@ const handleRegister = (req, res, db, bcrypt, saltRounds) => {
             joined: new Date()
           })
           .then((user) => {
-            res.json(user[0]);
+            // Crearea tokenului JWT
+            const token = jwt.sign(
+              { userId: user[0].id, email: user[0].email }, // Asigură-te că transmiți userId și email aici
+              JWT_SECRET, // Secretul pentru semnarea tokenului
+              { expiresIn: "1h" } // Tokenul expiră după o oră
+            );
+
+            // Trimite userul si token-ul către client
+            res.status(200).json({ user: user[0], token: token });
           });
       })
       .then(trx.commit)
